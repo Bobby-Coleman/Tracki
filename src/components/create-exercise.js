43198e6e20421dadd1b66d4e18cdffe082e93586
Bobby Coleman <colemankidding@gmail.com>
2020-05-28 16:40:51 -0700

    Handle input events

--- src/components/create-exercise.js
+++ src/components/create-exercise.js
@@ -1,6 +1,45 @@
 import React, { Component } from 'react';
 
 export default class CreateExercisesList extends Component {
+    
+    
+    
+    state = {
+            username: '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: [],
+        }
+
+    onChangeUsername(event) {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    onChangeDescription(event) {
+        this.setState({
+            description: event.target.value
+        })
+    }
+
+    onChangeDuration(event) {
+        this.setState({
+            duration: event.target.value
+        })
+    }
+
+    onChangeDate(date) {
+        this.setState({
+            username: date.target.value
+        })
+    }
+
+
+
+
+
     render() {
         return (
             <div>
